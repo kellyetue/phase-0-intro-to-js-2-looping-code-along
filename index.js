a352d8cfb228673e98e6948896127d4a5ee56db4
kellyetue@gmail.com <kellyetue@gmail.com>
2023-03-29 18:27:58 -0700

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, birthday) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return message; 
+}
+
+function countDown(positive) {
+    while (positive >= 0)  {
+        console.log(positive);
+        positive--;
+    }
+}
